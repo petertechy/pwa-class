c33b8a2c939f7e52522aa1b77e3fbf11f5d787b8
petertechy <petertechy01@gmail.com>
2024-12-10 23:13:16 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="mx-auto col-7 shadow p-5 my-4 border rounded-3">
+        <h1 className="display-1 text-center text-danger">
+          Hello Senior Devs
+        </h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, ipsa! Velit alias explicabo repellendus eum laudantium officiis dicta blanditiis perspiciatis similique facere illo numquam temporibus natus, nisi provident corporis! Esse, ad sapiente aspernatur at iure veritatis! Optio aspernatur quod qui, facilis ducimus perferendis! Explicabo corrupti sed impedit, nobis natus rerum, cumque ipsam eveniet aut exercitationem in odio ducimus perspiciatis eligendi eius tempore optio quia possimus minima, neque laboriosam quisquam? Rem, numquam dicta sequi quibusdam suscipit deserunt ab optio ea deleniti pariatur quod sint tenetur, asperiores distinctio blanditiis odit at recusandae? Natus voluptas nobis blanditiis, quae, accusantium sunt quas nisi autem tempora quod perferendis nam facilis quidem placeat expedita minus modi distinctio ipsa assumenda. Sapiente ex ipsum nisi corporis hic eius? Quidem dolorum totam, beatae corrupti id ab nostrum? Laborum exercitationem temporibus ratione, veritatis impedit, necessitatibus provident laudantium in aperiam nisi iure facilis odio dolor odit. Asperiores maiores tempora eum ea quos dignissimos consequuntur odio voluptate! Vero placeat assumenda nesciunt molestias tempore voluptas veniam dolorum ab quas nihil fuga praesentium quasi sed odio modi consectetur, debitis esse reprehenderit. Voluptas reprehenderit ullam consectetur neque mollitia rerum nam sit, inventore possimus itaque dignissimos ipsam similique, consequatur totam molestias quod alias vero laboriosam. Aut accusantium perferendis, quibusdam fugit autem quam incidunt asperiores ratione laborum, soluta illum natus numquam ullam libero adipisci aliquid et ducimus voluptatibus illo nostrum hic repellendus quis cum? Enim excepturi non voluptatibus architecto natus praesentium amet, velit deleniti voluptatem ullam, nostrum voluptas quis doloremque eum! Praesentium consequatur molestiae cum officiis quasi tempore, aperiam voluptate porro quo, et non deleniti magni facilis sapiente laborum beatae deserunt architecto! Error numquam asperiores iusto quos nihil saepe. Quo architecto, labore nesciunt atque ut, adipisci porro similique voluptatum assumenda, blanditiis nisi sunt nobis ea perferendis rem unde placeat quis facilis. Quis veritatis nihil odit laudantium. Ad ducimus harum ipsa est, quibusdam quas quisquam necessitatibus cumque rerum aliquam magni vero, tenetur molestiae! Molestias dolore, fugit provident in vitae commodi suscipit dolor, nostrum doloremque ea mollitia aspernatur? Mollitia quaerat quis vitae voluptates laborum, repellendus voluptas, porro nesciunt impedit voluptatibus eum tempore modi enim atque doloribus eaque laudantium nulla inventore quod necessitatibus quo, eveniet ad! Modi eius fuga, quibusdam ad, quod, adipisci repellat ullam repellendus aspernatur perspiciatis quam necessitatibus quaerat labore minus autem debitis magni! Soluta rem quaerat voluptas nostrum. Eius cupiditate ea dicta blanditiis aliquam eveniet vel veniam voluptatibus odio, corrupti quia harum corporis deleniti molestiae aut nobis.</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
